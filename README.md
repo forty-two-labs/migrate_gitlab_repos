3e91a9b8580740de017767dd83fb323e290c8328
Eddie Lau <tatonlto@gmail.com>
2015-02-08 14:33:03 +0800

    ruby script with explanation in README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # migrate_gitlab_repos
+
 ruby script to migrate git repos from one gitlab server and another gitlab server
+
+
+## Notes
+
+the script will create two folders in the current directories:
+
+* existing_repos
+* new_repos
+
+the script is basically doing:
+
+* create existing_repos directory
+* git clone the project repo
+* generate a git bundle
+* create new_repos directory
+* create new project repo on new git server
+* git clone the empty new project repo from new git server
+* pull the bundle from existing_repos generated
+* git push the changes to new git server
+
+
+## Parameters
+
+set the following values in the main.rb script:
+
+* GITLAB_NAMESPACE - the namespace of the repo
+* EXISTING_GITLAB - domain name or ip address of current gitlab server
+* NEW_GITLAB - domain name or ip address of new gitlab server
+* NEW_GITLAB_PRIVATE_TOKEN -  private token of new gitlab server (you can find it inside new gitlab web UI, Profile settings > Account)
+* NEW_GITLAB_NAMESPACE_ID - you have to create the namespace on the new server manually first (could be done via HTTP API, but I didn't that)
+* PROJECT_NAMES - array of project names that you want to migrate
---
+++ main.rb
@@ -0,0 +1,40 @@
+GITLAB_NAMESPACE=''
+
+EXISTING_GITLAB=''
+
+NEW_GITLAB=''
+NEW_GITLAB_PRIVATE_TOKEN=''
+NEW_GITLAB_NAMESPACE_ID=
+
+PROJECT_NAMES = %w(project-one project-two)
+
+def bundle_existing_repos(project_name)
+  Dir.mkdir "existing_repos"
+  Dir.chdir "existing_repos"
+  system "git clone git@#{EXISTING_GITLAB}:#{GITLAB_NAMESPACE}/#{project_name}.git "
+  Dir.chdir "#{project_name}"
+  system "git bundle create #{project_name}.bundle --all"
+  Dir.chdir ".."
+  Dir.chdir ".."
+end
+
+def create_new_repos_and_push_bundle(project_name)
+  Dir.mkdir "new_repos"
+  Dir.chdir "new_repos"
+
+  system "curl -H \"PRIVATE-TOKEN: #{NEW_GITLAB_PRIVATE_TOKEN}\" -H \"Content-Type:application/json\" http://#{NEW_GITLAB}/api/v3/projects -d '{ \"name\": \"#{project_name}\", \"namespace_id\": #{NEW_GITLAB_NAMESPACE_ID}, \"visibility_level\": 10 }'"
+  system "git clone git@#{NEW_GITLAB}:#{GITLAB_NAMESPACE}/#{project_name}.git "
+  Dir.chdir "#{project_name}"
+  system "git pull ../../existing_repos/#{project_name}/#{project_name}.bundle"
+  system "git push origin master"
+  Dir.chdir ".."
+end
+
+def migrate_project(project_name)
+  bundle_existing_repos(project_name)
+  create_new_repos_and_push_bundle(project_name)
+end
+
+PROJECT_NAMES.each do |project_name|
+  migrate_project(project_name)
+end
